31e6d3e7f4ce3c596345745a62c2ad0d1d31bf14
JNeaderland <144148081+JNeaderland@users.noreply.github.com>
2024-01-29 12:26:36 -0500

    Update Lab 11-5.py

--- Lab 11-5.py
+++ Lab 11-5.py
@@ -1,9 +1,9 @@
-name1 = input("whats your first name")
-name2 = input("whats your second name")
-name3 = input("whats your third name")
-name4 = input("whats your fourth name")
-name5 = input("whats your fifth name")
-print(name1.unique())
+nam = [input("whats your first name? "), input("whats your second name? "), input("whats your third name? "), input("whats your fourth name? "), input("whats your fifth name? ")]
+name1 = nam[0]
+name2 = nam[1]
+name3 = nam[2]
+name4 = nam[3]
+name5 = nam[4]
 def name_multiplication(n1, n2, n3, n4, n5):
     names = [n1, n2, n3, n4, n5]
     prints = 0
