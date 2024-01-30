e4e8cc198e809575824dba0fe698ab1234b5dccc
JNeaderland <144148081+JNeaderland@users.noreply.github.com>
2024-01-30 13:15:33 -0500

    Update Lab 11-6, Lab 11-7.py

--- Lab 11-6, Lab 11-7.py
+++ Lab 11-6, Lab 11-7.py
@@ -1,22 +1,6 @@
-"""
-
-Create a Python file named lab_10-1.py
-*** You must write a comment for every chunk of code you write from now on along with your author tag***
-
-Using a while loop, write a program that prompts the user to input a number.
-If the user inputs a number, add that to the sum of the previous numbers entered.
-If the user enters -1, the program should end and display the sum of all the numbers entered.
-Be sure the program uses a break statement
-_____________________________________________________________________________________________________________
-
-Create a Python file named lab_10-2.py
-Using the same approach as lab 1, write a program that prints all the numbers that are multiples of 3 in a list. Be sure your program uses a continue statemen
-
-
-"""
 
 #Author: Jacob Neaderland
-#Lab 10-1
+#Lab 11-6
 num = input("What is your number? ")
 numi = int(num)
 intn = int(0)
@@ -30,7 +14,7 @@ while type(numi) == type(intn): #Always active
         break
 print ("Sum equals ", nsum)
 
-#Lab 10-2
+#Lab 11-7
 num = "0"
 numi = int(0)
 intn = int(0)
